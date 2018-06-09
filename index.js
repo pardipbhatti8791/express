08246f67f768c05342afcdd3e230a3c35ef25929
Pardip Bhatti <pardipbhatti@gpcoders.com>
2018-06-10 04:00:58 +0530

    express js

---
+++ index.js
@@ -0,0 +1,83 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+    {
+        id: 1,
+        name: 'PHP'
+    },
+    {
+        id: 2,
+        name: 'React'
+    },
+    {
+        id: 3,
+        name: 'Node'
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('Hello gugu !!');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Not found');
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    // Get the course
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Not found');
+
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    // Update course
+    course.name = req.body.name;
+    res.send(course)
+});
+
+/**
+ * @param {Validating Course Data} course 
+ */
+function validateCourse(course) {
+    // Validate course
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
+
+// PORT
+const port = process.env.PORT || 9187;
+app.listen(port, () => {
+    console.log(`listing port ${port}`);
+});
